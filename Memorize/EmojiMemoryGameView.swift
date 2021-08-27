e725736c664fefc7481df7fa15c89086a89cf723
Galil <v.calil@hotmail.com>
2021-08-26 20:12:12 -0700

    Alteracao da View, deixando o Grid dinamico

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -14,28 +14,41 @@ struct EmojiMemoryGameView: View {
     //@State var length = viewModel.cards
     
     var body: some View {
-        VStack {
-            ScrollView {
-                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
-                    ForEach(game.cards) { card in
-                        CardView(card: card).aspectRatio(2/3, contentMode: .fit).onTapGesture{
-                            game.choose(card: card)
-                        }
-                        
+//        VStack {
+//            ScrollView {
+//                LazyVGrid(columns: [GridItem(.adaptive(minimum: 100))]) {
+//                    ForEach(game.cards) { card in
+        AspectVGrid(items: game.cards, aspectRatio: 2/3, content: { card in
+            if card.isMatched && !card.isFaceUp {
+                Rectangle().opacity(0)
+            }else {
+                CardView(card: card)
+                    .padding(4)
+                    .onTapGesture {
+                    game.choose(card: card)
                     }
-                }
-            }.foregroundColor(.red)
-        }.padding(.horizontal)
-        Spacer()
-        HStack {
-            Button {
-                shuffle(model: game)
-            } label: {
-                    Image(systemName: "minus.circle")
-                }
-        }
-        .font(.largeTitle)
+            }
+        })
+        .foregroundColor(.red)
+        //.font(.largeTitle)
         .padding(.horizontal)
+//
+//
+//                    }
+//                }
+//            }.foregroundColor(.red)
+//        }.padding(.horizontal)
+//        Spacer()
+//        HStack {
+//            Button {
+//                shuffle(model: game)
+//            } label: {
+//                    //Image(systemName: "minus.circle")
+//                    Text("Shuffle")
+//                }
+//        }
+//        .font(.largeTitle)
+//        .padding(.horizontal)
 
     }
     
@@ -70,9 +83,9 @@ struct CardView: View {
     }
     
     private struct DrawingConstants {
-        static let cornerRadius: CGFloat = 20
+        static let cornerRadius: CGFloat = 10
         static let lineWidth: CGFloat = 3
-        static let fontScale: CGFloat = 0.8
+        static let fontScale: CGFloat = 0.75
     }
 }
 /*
