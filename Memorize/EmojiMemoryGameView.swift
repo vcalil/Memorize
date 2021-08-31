86a3118e7d0f33ec486d15d0761284d7b23dc56f
Galil <v.calil@hotmail.com>
2021-08-30 21:16:10 -0700

    Inserindo animacao para pares iguais

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -32,6 +32,14 @@ struct EmojiMemoryGameView: View {
         .foregroundColor(.red)
         //.font(.largeTitle)
         .padding(.horizontal)
+        Spacer()
+        HStack {
+            Button {
+                shuffle(model: game)
+            } label: {
+                Text("Shuffle")
+            }
+        }
 //
 //
 //                    }
@@ -67,18 +75,28 @@ struct CardView: View {
                 Pie(startAngle: Angle(degrees: 0-90), endAngle: Angle(degrees: 110-90))
                     .padding(5)
                     .opacity(0.5)
-                Text(card.content).font(font(in: geometry.size))
+                Text(card.content)
+                    //.font(font(in: geometry.size))
+                    .rotationEffect(Angle.degrees(card.isMatched ? 360 : 0))
+                    .animation(Animation.linear(duration: 1).repeatForever(autoreverses: false))
+                    .font(Font.system(size: DrawingConstants.fontSize))
+                    .scaleEffect(scale(thatFits: geometry.size))
             }
             .cardify(isFaceUp: card.isFaceUp)
         }
         
     }
-    private func font(in size: CGSize) -> Font {
-        Font.system(size: min(size.width , size.height) * DrawingConstants.fontScale)
+//    private func font(in size: CGSize) -> Font {
+//        Font.system(size: min(size.width , size.height) * DrawingConstants.fontScale)
+//    }
+    
+    private func scale(thatFits size: CGSize) -> CGFloat {
+        min(size.width, size.height) / (DrawingConstants.fontSize / DrawingConstants.fontScale)
     }
     
     private struct DrawingConstants {
         static let fontScale: CGFloat = 0.7
+        static let fontSize: CGFloat = 32
     }
 }
 /*
